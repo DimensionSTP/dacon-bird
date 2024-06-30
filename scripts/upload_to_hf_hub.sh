3217eaacdd80c4987788baf67277437ea503abe7
DimensionSTP <ddang8jh@gmail.com>
2024-07-01 07:00:57 +0900

    add: basic upload model scripts

---
+++ scripts/upload_to_hf_hub.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+path="src/postprocessing"
+is_tuned="untuned"
+strategy="ddp"
+upload_user="microsoft"
+model_type="swinv2-large-patch4-window12to16-192to256-22kto1k-ft"
+precision=32
+batch_size=24
+epoch=10
+
+python $path/upload_to_hf_hub.py \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    precision=$precision \
+    batch_size=$batch_size \
+    epoch=$epoch
