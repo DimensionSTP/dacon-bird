5aaa0b71f11fe58bd7376d8f4c0f97b57b6cc28a
DimensionSTP <ddang8jh@gmail.com>
2024-12-24 18:37:01 +0900

    add: prepare and upload all scripts

---
+++ scripts/prepare_upload_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+path="src/postprocessing"
+is_tuned="untuned"
+strategy="ddp"
+upload_user="microsoft"
+model_type="swinv2-large-patch4-window12to16-192to256-22kto1k-ft"
+precision=32
+batch_size=24
+model_detail="swinv2-large-patch4-window12to16-192to256-22kto1k-ft"
+
+python $path/prepare_upload_all.py \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    precision=$precision \
+    batch_size=$batch_size \
+    model_detail=$model_detail
